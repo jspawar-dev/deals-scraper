ee20743d909e150e31e4b1f14b85b5afbb49c040
Jaskaranbir Singh Pawar <contact@jspawar.com>
2024-10-14 18:22:51 +0100

    Update items.py

--- tesco-scrapy/dealscraper/items.py
+++ tesco-scrapy/dealscraper/items.py
@@ -7,19 +7,20 @@ import scrapy
 from itemloaders.processors import TakeFirst, MapCompose
 from w3lib.html import remove_tags
 
-
+# removes currency symbols to clean the data before its stored.
 def remove_currency(value):
     if not None:
         return value.replace('£', '').strip()
     else:
         return '0.00'
 
-
+# the href link scraped does not include the domain. so here we complete the link before its stored.
 def tesco_complete_link(value):
     return 'https://www.tesco.com' + value
 
 
 # add remove_currency to price
+# This allows us to create a an item which is like a python dictionary.
 class TescoItem(scrapy.Item):
     name = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
     price = scrapy.Field(input_processor=MapCompose(remove_tags, remove_currency), output_processor=TakeFirst())
